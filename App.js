8a87e292c89e091b11a3b768f760fddd45d9a730
NicolasBonals <117324905+NicolasBonals@users.noreply.github.com>
2024-01-25 02:06:16 -0300

    Initial commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={{paddingTop: 20, paddingLeft: 30}}>
+      <Text>Hola Coder!</Text>
+      <Text>Empecemos con esto!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
